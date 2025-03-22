52197bada8db5ba564d51a0c0eae6e3037ae8c08
Spagheta <38372778+Spagheta-DSi@users.noreply.github.com>
2025-03-22 16:08:06 +0200

    Corrected the PCI registers

--- src/machine/m_at_socket4.c
+++ src/machine/m_at_socket4.c
@@ -97,12 +97,12 @@ machine_at_acerm3_init(const machine_t *model)
     machine_at_common_init(model);
 
     pci_init(PCI_CONFIG_TYPE_2 | PCI_NO_IRQ_STEERING);
-    pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
-    pci_register_slot(0x01, PCI_CARD_IDE,         0, 0, 0, 0);
-    pci_register_slot(0x0f, PCI_CARD_NORMAL,      1, 2, 3, 4);
-    pci_register_slot(0x0c, PCI_CARD_NORMAL,      2, 3, 4, 1);
-    pci_register_slot(0x0b, PCI_CARD_NORMAL,      3, 4, 1, 2);
-    pci_register_slot(0x02, PCI_CARD_SOUTHBRIDGE, 0, 0, 0, 0);
+    pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE,   0, 0, 0, 0);
+    pci_register_slot(0x01, PCI_CARD_NORMAL,        1, 4, 3, 2);
+    pci_register_slot(0x02, PCI_CARD_NORMAL,        2, 1, 4, 3);
+    pci_register_slot(0x03, PCI_CARD_NORMAL,        3, 2, 1, 4);
+    pci_register_slot(0x04, PCI_CARD_IDE,           4, 0, 0, 0);
+    pci_register_slot(0x05, PCI_CARD_SOUTHBRIDGE,   0, 0, 0, 0);
     device_add(&i430lx_device);	
     device_add(&sio_zb_device);	
     device_add(&keyboard_ps2_acer_pci_device);
